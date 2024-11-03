c77b56e562dd64d4b80fb61f122cb396a3656101
srinivaspanchal <vadlasrinivas01@gmail.com>
2024-11-03 23:12:16 +0530

    new application added to repo

---
+++ src/main/java/com/srinu/main/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.srinu.main;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(S3demoApplication.class);
+	}
+
+}
